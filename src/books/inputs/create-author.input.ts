47da13b665c4102573f930227a1beca098aadc85
ahmady016 <ahmady09@gmail.com>
2021-12-17 22:34:51 +0200

    Add Validation when create/update [author, book]

--- src/books/inputs/create-author.input.ts
+++ src/books/inputs/create-author.input.ts
@@ -1,20 +1,28 @@
 import { InputType, Field } from '@nestjs/graphql'
+import { IsDateString, IsEnum, IsString, Length } from 'class-validator'
 import { Gender } from 'src/__common/types'
-
 @InputType()
 export class CreateAuthorInput {
   @Field(() => String)
+  @IsString()
+  @Length(2, 50)
   firstName: string
 
   @Field(() => String)
+  @IsString()
+  @Length(5, 100)
   lastName: string
 
   @Field(() => String)
+  @IsEnum(Gender)
   gender: Gender
 
   @Field(() => String)
+  @IsDateString()
   birthDate: Date
 
   @Field(() => String)
+  @IsString()
+  @Length(2, 100)
   country: string
 }
--- src/books/inputs/create-book.input.ts
+++ src/books/inputs/create-book.input.ts
@@ -1,37 +1,73 @@
-import { InputType, Int, Field } from '@nestjs/graphql'
-
+import { InputType, Field } from '@nestjs/graphql'
+import {
+  IsString,
+  Length,
+  IsNumber,
+  Min,
+  Max,
+  IsArray,
+  ArrayMaxSize,
+  ArrayMinSize,
+} from 'class-validator'
+import { Type } from 'class-transformer'
 @InputType()
 export class CreateBookInput {
   @Field(() => String)
+  @IsString()
+  @Length(5, 70)
   title: string
 
   @Field(() => String)
+  @IsString()
+  @Length(10, 120)
   subtitle: string
 
   @Field(() => String)
+  @IsString()
+  @Length(20, 1000)
   description: string
 
   @Field(() => String)
+  @IsString()
+  @Length(5, 50)
   publisher: string
 
   @Field(() => String)
+  @IsString()
+  @Length(2, 50)
   language: string
 
   @Field(() => String)
+  @IsString()
+  @Length(2, 50)
   country: string
 
   @Field(() => String)
+  @IsString()
+  @Length(10, 500)
   imageURL: string
 
   @Field(() => String)
+  @IsString()
+  @Length(10, 500)
   infoURL: string
 
   @Field(() => Number)
+  @IsNumber()
+  @Min(1900)
+  @Max(new Date().getFullYear())
   publishedYear: number
 
   @Field(() => Number)
+  @IsNumber()
+  @Min(100)
+  @Max(10000)
   pageCount: number
 
   @Field(() => [String])
+  @IsArray()
+  @ArrayMinSize(1)
+  @ArrayMaxSize(10)
+  @Type(() => String)
   authorsIds: string[]
 }
